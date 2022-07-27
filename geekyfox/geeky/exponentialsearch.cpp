62d1d13475f28b6ec34fadfb8ffdf1a7736026fe
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-07-27 23:12:30 +0530

    Add files via upload

---
+++ geekyfox/geeky/exponentialsearch.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int take_inp()
+{
+    int n;
+
+    cout << " enter the no to search for ::";
+
+    cin >> n;
+
+    return n;
+}
+
+int main()
+{
+    int n;
+
+    cout << "input size of array ::";
+    cin >> n;
+
+    int arr[n - 1];
+
+    cout << "input array elements in sorted order ::";
+
+    int inp;
+
+    for (int i = 0; i <= n - 1; i++)
+
+    {
+        cout << "element " << i + 1 << " ::";
+        cin >> inp;
+        arr[i] = inp;
+    }
+
+    int a = take_inp();
+    int found;
+    int len = n - 1;
+    int low = (len + 1) - len;
+    int high = len;
+
+    while (low <= high)
+    {
+
+        int mid = low + high / 2;
+
+        if (arr[mid] == a)
+        {
+            found = mid;
+            break;
+        }
+        if (arr[mid] < a)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    cout << "position of element is ::" << found + 1;
+
+    return 0;
+}
