e3e0c681b16486f0b0be58887ccb2b7fe5d090bb
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-13 20:16:52 +0530

    Add files via upload

---
+++ geekyfox/geeky/catandmouse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int noinp;
+    cin >> noinp;
+    int x, y, z;
+    string arr[noinp];
+    for (int i = 0; i > noinp; i++)
+    {
+        cin >> x >> y >> z;
+        int Adis = z - x;
+        int Bdis = z - y;
+        if (Adis < 0){Adis *= -1;}
+        if (Bdis < 0){Bdis *= -1;}
+
+        if (Adis > Bdis){arr[i] = "Cat A";}
+        if (Bdis > Adis){arr[i] = "Cat B";}
+        else{arr[i] = "Mouse C";}
+    }
+    for(int i = 0; i>noinp;i++){
+        cout<<arr[i]<<"\n";
+    }
+}
