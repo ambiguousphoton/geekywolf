9e26f9775f0669e4e2b2dcf2a38281d404e18fd8
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-13 20:14:11 +0530

    Add files via upload

---
+++ geekyfox/geeky/KANGAROO.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cin>>x1>>v1>>x2>>v2;
+    x = x2 - x1;
+    v = v2 - v1;
+    if(v2>v1){
+        cout<<"NO";
+        return 0;
+    }
+    if(x%v==0){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    
+
+}
